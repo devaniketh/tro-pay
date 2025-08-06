1e055497b4151224acd2df927e32b06a4c1ae773
aniketh <techrunstheworld@gmail.com>
2025-08-07 00:11:59 +0530

    feat: wallet-Provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import ConnectButton from "@/components/ConnectButton";
+import { div } from "motion/react-client";
 
 export default function Home() {
   return (
-    <div >
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Tropay </h1>
+      <ConnectButton />
     </div>
-  );
+  )
 }
---
+++ components/WalletProvider.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { ReactNode, useMemo } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { clusterApiUrl } from '@solana/web3.js'
+
+export default function WalletConnectionProvider({ children }: { children: ReactNode }) {
+  const endpoint = clusterApiUrl('devnet')
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
